aaaa431076d91552179a83f18315643f2fa40a2a
Matthew Alexander LaChance <mattlach@umich.edu>
2018-02-24 18:51:19 -0500

    :white_check_mark: PrimeJob#volumePaths returns a path

--- spec/dev/primeocr-job.spec.js
+++ spec/dev/primeocr-job.spec.js
@@ -3,18 +3,21 @@
 // BSD License. See LICENSE.txt for details.
 
 /* eslint-env mocha */
-/* eslint-disable no-unused-vars */
 const expect = require("chai").expect;
 const PrimeJob = require("../../lib/primeocr-job");
 
+let job;
+
 describe("PrimeJob with only 00000001.tif", () => {
   beforeEach(() => {
-    PrimeJob("Prime Recognition Job File\n"
-             + "Version 3.90\n"
-             + "1\n"
-             + "o:\\vol\\path\\00000001.tif\n"
-             + "o:\\templates\\whocares.ptm\n");
+    job = PrimeJob("Prime Recognition Job File\n"
+                   + "Version 3.90\n"
+                   + "1\n"
+                   + "o:\\vol\\path\\00000001.tif\n"
+                   + "o:\\templates\\whocares.ptm\n");
   });
 
-  it("runs", () => {});
+  it("knows to look in the right volume path", () => {
+    expect(job.volumePaths()).to.deep.equal(["vol/path"]);
+  });
 });
