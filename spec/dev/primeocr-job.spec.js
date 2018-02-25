aaaacf5c7e7c58bdcc111bcd0950a3358d251f6d
Matthew Alexander LaChance <mattlach@umich.edu>
2018-02-25 17:16:43 -0500

    :white_check_mark: Throw error on invalid template paths

--- spec/dev/primeocr-job.spec.js
+++ spec/dev/primeocr-job.spec.js
@@ -39,6 +39,16 @@ describe("PrimeJob", () => {
     }).to.throw(Error);
   });
 
+  it("fails when given an invalid template path", () => {
+    expect(() => {
+      PrimeJob("Prime Recognition Job File\n"
+               + "Version 3.90\n"
+               + "1\n"
+               + "o:\\vol\\path\\00000001.tif\n"
+               + "o:templates\\whocares.ptm\n");
+    }).to.throw(Error);
+  });
+
   it("can handle CRLF jobfiles", () => {
     expect(() => {
       PrimeJob("Prime Recognition Job File\r\n"
