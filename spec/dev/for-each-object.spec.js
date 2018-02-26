aaaa0b6d720bb1141c288d975ce1dc0e63e509d9
Matthew Alexander LaChance <mattlach@umich.edu>
2018-02-26 06:22:29 -0500

    :white_check_mark: forEachIn() exists

---
+++ spec/dev/for-each-object.spec.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018 The Regents of the University of Michigan.
+// All Rights Reserved. Licensed according to the terms of the Revised
+// BSD License. See LICENSE.txt for details.
+
+/* eslint-env mocha */
+/* eslint-disable no-unused-vars */
+const expect = require("chai").expect;
+const forEachIn = require("../../lib/for-each-object");
