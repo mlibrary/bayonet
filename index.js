aaaa6e99a1692a2e7707cd14dadab34a5f9d2ef0
Matthew Alexander LaChance <mattlach@umich.edu>
2018-02-26 11:18:29 -0500

    :white_check_mark: Add PrimeJob to index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// Copyright (c) 2017 The Regents of the University of Michigan.
+// Copyright (c) 2017-2018 The Regents of the University of Michigan.
 // All Rights Reserved. Licensed according to the terms of the Revised
 // BSD License. See LICENSE.txt for details.
 
 module.exports = {
+  "PrimeJob": require("./lib/primeocr-job"),
   "SelectionList": require("./legacy/selection-lists")
 };
--- spec/dev/primeocr-job.spec.js
+++ spec/dev/primeocr-job.spec.js
@@ -20,7 +20,7 @@
 /* eslint-env mocha */
 /* eslint-disable no-unused-expressions */
 const expect = require("chai").expect;
-const PrimeJob = require("../../lib/primeocr-job");
+const PrimeJob = require("../..").PrimeJob;
 
 let job;
 
